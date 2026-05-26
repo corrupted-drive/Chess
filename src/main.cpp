b2f9de941df510f995386ca1f0ed531d9d28744a
Himansh Mewada <himanshmewada008@gmail.com>
2026-05-26 09:54:38 +0530

    Simple Window Created

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  sf::RenderWindow window{sf::VideoMode{{800, 600}}, "Chess"};
+  while (window.isOpen()) {
+    while (const std::optional event{window.pollEvent()}) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    window.clear(sf::Color{254, 22, 32});
+
+    window.display();
+  }
+
+  return 0;
 }
